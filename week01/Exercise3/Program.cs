e3afb670a13876e49b8a9a934b948e68f78516e2
littlemissmiso <todayisdelightful@gmail.com>
2025-03-05 21:10:15 -0700

    Magic Number Guess Program

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,31 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine();
+
+        int magicNumber = 4;
+
+        int numberGuess = -1;
+
+        while (magicNumber != numberGuess) {
+
+            Console.Write("Please guess the magic number: ");
+            numberGuess = int.Parse(Console.ReadLine());
+
+            if (numberGuess == magicNumber) {
+                Console.WriteLine("Congratulations, you guessed it!");
+                break;
+            }
+            else if (numberGuess > magicNumber) {
+                Console.WriteLine("Lower.");
+            }
+            else {
+                Console.WriteLine("Higher.");
+            }
+
+        }
+
+        
+
     }
 }
