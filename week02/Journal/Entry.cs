ead1a94f679b08af74ce6fc31a443a4e886e3877
littlemissmiso <todayisdelightful@gmail.com>
2025-03-12 20:19:39 -0600

    Journal Program Outline

---
+++ week02/Journal/Entry.cs
@@ -0,0 +1,11 @@
+public class Entry 
+{
+    public string _entryDate;
+    public string _entryPrompt;
+    public string _entryText;
+
+    public void Display()
+    {
+        
+    }
+}
---
+++ week02/Journal/PromptGenerator.cs
@@ -0,0 +1,10 @@
+public class PromptGenerator 
+{
+    public List<string> _prompts;
+
+    public string GetRandomPrompt()
+    {
+        /*Throws an error if completely blank, so row below is a placeholder*/
+        return "";
+    }
+}
